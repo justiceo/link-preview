156326c536220f4f98fe29d501c94b2053e830ef
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-01-01 05:50:13 -0800

    Add polish to the welcome page experience including demo

---
+++ src/assets/welcome/script.js
@@ -0,0 +1,6 @@
+document.getElementById("demo-button").onclick = function (e) {
+  window.postMessage(
+    { application: "better-previews", action: "search", data: "hello world" },
+    window.location.origin
+  );
+};

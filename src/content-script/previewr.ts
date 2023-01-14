f6e144751575f97cb3794e4dcb6bbd180c16644f
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-01-13 20:03:58 -0800

    Add winbox for previews

--- src/content-script/previewr.ts
+++ src/content-script/previewr.ts
@@ -1,11 +1,17 @@
 import {Logger} from '../logger';
+import WinBox from "winbox/src/js/winbox";
+import "winbox/dist/css/winbox.min.css";
+import {sanitizeUrl} from '@braintree/sanitize-url';
 
 // This class is responsible to loading/reloading/unloading the angular app into the UI.
 export class Previewr {
   getExtensionUrl = chrome.runtime.getURL;
   logger = new Logger("previewr");
-
+  headerIconUrlBase = 'https://www.google.com/s2/favicons?domain=';
+  dialog?: WinBox;
   isVisible = false;
+  url: URL;
+  navStack: URL[] = [];
 
   /* This function inserts an Angular custom element (web component) into the DOM. */
   init() {
@@ -47,10 +53,63 @@ export class Previewr {
           return;
         }
 
-        this.logger.log('#WindowMessage: ', event);        
+        this.logger.log('#WindowMessage: ', event); 
+        this.handleMessage(event.data);       
       },
       false
     );
+  }
+
+  handleMessage(message) {
+    // Extract the url from the message.
+    let urlStr;
+    if (message.action === 'copy') {
+      navigator.clipboard.writeText(message.data);
+      return;
+    } else if (message.action === 'preview') {
+      urlStr = message.data;
+    } else if (message.action === 'search') {
+      urlStr = 'https://google.com/search?igu=1&q=' + message.data;
+    } else if (message.action === 'load') {
+      if (message.sourceFrame === 'iframer') {
+        this.dialog.title = "header update";
+      }
+    } else if (message.action === 'navigate') {
+      urlStr = message.href;
+    } else {
+      this.logger.warn('Unhandled action', message);
+    }
+
+    // Ensure it is valid.
+    if (!urlStr || sanitizeUrl(urlStr) === 'about:blank') {
+      return;
+    }
+    let newUrl;
+    try {
+      newUrl = new URL(urlStr);
+    } catch (e) {
+      this.logger.error(e);
+      return;
+    }
+
+    // Move the old URL to backstack.
+    if (this.url && this.url.href !== newUrl.href) {
+      this.navStack.push(this.url);
+    }
+
+    // Preview new URL.
+    this.url = newUrl;
+    this.previewUrl(this.url);
+  }
+
+  previewUrl(url: URL) {
+    this.logger.log("#previewUrl: ", url);
+   
+    this.dialog = new WinBox(url.hostname, {
+      icon: this.headerIconUrlBase + url.hostname,
+      url: url.href,
+      border: 2,
+    });
   }
 
   /*

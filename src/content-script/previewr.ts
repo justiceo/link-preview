55bf11ee17a939442496c8702fd928ada9c68d19
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-01-16 02:01:43 -0800

    Fix template concatenation inside template literal

--- src/content-script/previewr.ts
+++ src/content-script/previewr.ts
@@ -29,29 +29,29 @@ WinBox.prototype.getDom = function(){
 
 const template = document.createElement("div");
 template.innerHTML = `
-'<div class=wb-header>' +
-'<div class=wb-control>' +
-    '<span class=wb-min></span>' +
-    '<span class=wb-max></span>' +
-    '<span class=wb-full></span>' +
-    '<span class=wb-close></span>' +
-'</div>' +
-'<div class=wb-drag>'+
-    '<div class=wb-icon></div>' +
-    '<div class=wb-title></div>' +
-'</div>' +
-'</div>' +
+<div class=wb-header>
+<div class=wb-control>
+    <span class=wb-min></span>
+    <span class=wb-max></span>
+    <span class=wb-full></span>
+    <span class=wb-close></span>
+</div>
+<div class=wb-drag>
+    <div class=wb-icon></div>
+    <div class=wb-title></div>
+</div>
+</div>
 
-'<div class=wb-body></div>' +
+<div class=wb-body></div>
 
-'<div class=wb-n></div>' +
-'<div class=wb-s></div>' +
-'<div class=wb-w></div>' +
-'<div class=wb-e></div>' +
-'<div class=wb-nw></div>' +
-'<div class=wb-ne></div>' +
-'<div class=wb-se></div>' +
-'<div class=wb-sw></div>'
+<div class=wb-n></div>
+<div class=wb-s></div>
+<div class=wb-w></div>
+<div class=wb-e></div>
+<div class=wb-nw></div>
+<div class=wb-ne></div>
+<div class=wb-se></div>
+<div class=wb-sw></div>
 `
 // This class is responsible to loading/reloading/unloading the angular app into the UI.
 export class Previewr {

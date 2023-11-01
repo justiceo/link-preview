6a0eb5fbd6a3982f7b28fe2f2608bf4a7510fa69
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-10-31 20:11:58 -0700

    Switch console to logger

--- src/content-script/content-script.ts
+++ src/content-script/content-script.ts
@@ -1,19 +1,20 @@
 import { Floatie } from "./floatie/floatie";
 import { IFrameHelper } from "./iframe-helper";
 import { Previewr } from "./previewr";
+import { Logger } from "../utils/logger";
 
 class ContentScript {
   floatie = new Floatie();
   iframeHelper = new IFrameHelper();
   previewr = new Previewr();
+  logger = new Logger(this);
   unsupportedHosts = ["mail.google.com"];
 
   constructor() {}
 
   start() {
     if (this.isDisabledDomain()) {
-      console.warn("Better Previews is disabled on ", window.location.host);
-      // TODO: Update extension icon to gray.
+      this.logger.warn("Better Previews is disabled on ", window.location.host);
       return;
     }
 
@@ -22,7 +23,7 @@ class ContentScript {
     this.iframeHelper.registerListeners();
 
     chrome.runtime.onMessage.addListener((request, sender, callback) => {
-      console.debug("Re-posting message for DOM: ", request);
+      this.logger.debug("Re-posting message for DOM: ", request);
       window.postMessage({ application: "better-previews", ...request });
       callback("ok");
     });
@@ -39,6 +40,3 @@ class ContentScript {
 
 const cs = new ContentScript();
 cs.start();
-
-// There are many iframes in a window. Uncomment below to see.
-// console.log("cs: initialized on window:", window.location)

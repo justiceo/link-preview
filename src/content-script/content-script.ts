04785a90f2d762fc18451b462e1a8daa6b14c420
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-10-31 20:10:22 -0700

    Remove legacy sentry

--- src/content-script/content-script.ts
+++ src/content-script/content-script.ts
@@ -1,21 +1,6 @@
 import { Floatie } from "./floatie/floatie";
 import { IFrameHelper } from "./iframe-helper";
 import { Previewr } from "./previewr";
-import * as Sentry from "@sentry/browser";
-import { BrowserTracing } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://59000502bf014222a97c0b8d3ff7495e@o526305.ingest.sentry.io/4504220162719744",
-
-  integrations: [new BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0.1,
-  release: "better-previews@23.01.13",
-  environment: "PROD",
-});
 
 class ContentScript {
   floatie = new Floatie();

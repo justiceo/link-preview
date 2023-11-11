c58a6eae4a882b016087d0244f6d5aca586e8378
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-11-11 11:23:27 -0800

    [23.11.11] Create new prod version

--- build/chrome-prod/popup/popup.js
+++ build/chrome-prod/popup/popup.js
@@ -1,1 +1,2 @@
+var IS_DEV_BUILD=false;
 (()=>{document.querySelector("#go-to-options").addEventListener("click",()=>{chrome.runtime.openOptionsPage?chrome.runtime.openOptionsPage():window.open(chrome.runtime.getURL("options.html"))});})();

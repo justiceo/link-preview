d7d68b6f22dd1f8b45e7e64889e8269186695e75
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-11-11 01:57:49 -0800

    Update options page to use bootstrap, add preview and issues button.

--- src/options-page/options.ts
+++ src/options-page/options.ts
@@ -4,24 +4,11 @@ import "./options.css";
 
 const configOptions: Config[] = [
   {
-    id: "automatically-hide-previews",
-    type: "switch",
-    title: "Automatically hide previews",
-    description: "The detail information about the radio here.",
-    default_value: false,
-  },
-  {
-    id: "hide-previews-delay",
-    type: "range",
-    title: "How long to wait before automatically hiding previews (in seconds)",
-    description: "The detail information about the range here.",
-    default_value: 3,
-  },
-  {
-    id: "default-search-engine",
+    id: "search-engine",
     type: "select",
-    title: "Set default search engine",
-    description: "The detail information about the switch here.",
+    title: "Search engine",
+    description:
+      "The search engine that would be used for inline search previews.",
     default_value: "google",
     options: [
       { id: "google", text: "Google" },
@@ -32,24 +19,45 @@ const configOptions: Config[] = [
     ],
   },
   {
-    id: "show-copy-action",
+    id: "automatically-hide-previews",
     type: "switch",
-    title: "Show copy action",
-    description: "The detail information about the select here.",
+    title: "Automatically hide previews",
+    description:
+      "Hides the preview panel when you hit ESC, scroll away or interact with the main page.",
     default_value: false,
   },
   {
     id: "preview-on-hover",
     type: "switch",
-    title: "Display preview on hover (no need to click)",
-    description: "The detail information about the range here.",
+    title: "Display preview on hover",
+    description:
+      "No need to click on tooltip button, auto show preview after 3secs.",
     default_value: false,
+  },
+  {
+    id: "previewr-width",
+    type: "range",
+    title: "Preview Width (%)",
+    description: "The width of the preview panel relative to the page.",
+    default_value: 40,
+    min: "20",
+    max: "100",
+  },
+  {
+    id: "previewr-height",
+    type: "range",
+    title: "Preview Height (%)",
+    description: "The height of the preview panel relative to the page.",
+    default_value: 70,
+    min: "20",
+    max: "100",
   },
   {
     id: "sync-settings",
     type: "switch",
     title: "Enable chrome sync",
-    description: "The detail information about the textarea here.",
+    description:
+      "Sync these settings across Chrome browsers where you're signed in.",
     default_value: false,
   },
 ];
@@ -75,6 +83,16 @@ if (window.name === iframeName) {
   };
 }
 
-document
-  .querySelector(".options-container")
-  ?.appendChild(new SettingsUI(configOptions));
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector(".options-container")
+    ?.appendChild(new SettingsUI(configOptions));
+
+  document.querySelector("#show-preview")?.addEventListener("click", () => {
+    window.postMessage(
+      { application: "better-previews", action: "search", data: "hello world" },
+      window.location.origin
+    );
+  });
+
+});

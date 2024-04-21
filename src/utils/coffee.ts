e69d3dbda245ac02401b08a92eadd7b77466aacd
Justice Ogbonna <justiceo@users.noreply.github.com>
2024-04-21 11:42:31 -0700

    Make utils directory a submodule

--- src/utils/coffee.ts
+++
@@ -1,9 +0,0 @@
-import ExtPay from "./ExtPay";
-import { extensionId } from "./i18n";
-
-export class Coffee {
-  run() {
-    const extpay = ExtPay(extensionId);
-    extpay.startBackground();
-  }
-}
